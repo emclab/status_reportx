ff9061ef14286b7b5a436e41170e8f803a133546
jc <emclab2011@gmail.com>
2015-10-22 08:18:32 -0500

    JC: add before_action for index pagination page. spec passes.

--- app/controllers/status_reportx/application_controller.rb
+++ app/controllers/status_reportx/application_controller.rb
@@ -13,6 +13,7 @@ module StatusReportx
     before_action :load_session_variable, :only => [:new, :edit]  #for parent_record_id & parent_resource in check_access_right
     after_action :delete_session_variable, :only => [:create, :update]   #for parent_record_id & parent_resource in check_access_right
     before_action :view_in_config?
+    before_action :page_params, :only => :index
     
     protected
   
